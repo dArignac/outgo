d9c7dcb437ea9b5bdb873c5871b6e96a2b6631e6
Alexander Herrmann <darignac@gmail.com>
2016-09-05 16:56:35 +0200

    tour of heroes part 6

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,19 @@
+import './rxjs-extensions';
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }        from './app.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { HeroService }         from './hero.service';
 import { routing }             from './app.routing';
 
@@ -14,13 +22,16 @@ import { routing }             from './app.routing';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   providers: [
     HeroService
